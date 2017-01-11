031d5ed5dceba6cf1b920ecf35423f982b054ed3
Maxwell-lt <maxwell.lt@live.com>
2017-01-11 13:12:42 -0500

    Convert into module
    Now can be imported as a module in addition to being run as a script.
    
    Will wait for user interaction after giving the output in script mode, to allow the script to be run from a file explorer.

--- Toehold Generator.py
+++ Toehold Generator.py
@@ -1,30 +1,46 @@
-seq = input("Enter the MiRNA you want to target:").upper()
+#!/usr/bin/env python3
 
+def GenerateToehold(mirna):
+    """
+    Finds an RNA sequence that will unfold in the presence of the MiRNA sequence given as an argument.
+    
+    The format of the given string should consist of only the characters 'A', 'U', 'C', and 'G'. Any other character will be ignored.
+    Do not use 'T' in place of 'U'; the script will not make the conversion automatically.
+    """
 # Helper variable
 # REVerse COMPlimentary sequence
-revcomp = ""
+    revcomp = ""
 
-for i in seq:
-    if i == "A": revcomp += "U"
-    if i == "U": revcomp += "A"
-    if i == "C": revcomp += "G"
-    if i == "G": revcomp += "C"
+    for i in mirna.upper():
+        if i == "A": revcomp += "U"
+        if i == "U": revcomp += "A"
+        if i == "C": revcomp += "G"
+        if i == "G": revcomp += "C"
 
-# Reverses the string
-revcomp = revcomp[::-1]
+    # Reverses the string
+    revcomp = revcomp[::-1]
 
-yside = ""
+    yside = ""
 
-# Retrieves 10 characters of the reverse complimentary sequence for one of the two customized sections of the final RNA
-for i in revcomp[-10:]:
-    if i == "A": yside += "U"
-    if i == "U": yside += "A"
-    if i == "C": yside += "G"
-    if i == "G": yside += "C"
+    # Retrieves 10 characters of the reverse complimentary sequence for one of the two customized sections of the final RNA
+    for i in revcomp[-10:]:
+        if i == "A": yside += "U"
+        if i == "U": yside += "A"
+        if i == "C": yside += "G"
+        if i == "G": yside += "C"
 
 
-# Inserts customized portions into the proven toehold sequence boilerplate
-final = "GGG" + revcomp[-21:] + "AUGAUUGUAAACAGAGGAGAUACAAUAUG" +\
+    # Inserts customized portions into the proven toehold sequence boilerplate
+    final = "GGG" + revcomp[-21:] + "AUGAUUGUAAACAGAGGAGAUACAAUAUG" +\
         yside + "ACCUGGCGGCAGCGCAAAAGAUGCGUAAA"
+    
+    return final
 
-print(final)
+# If run as a script
+if __name__ == '__main__':
+    seq = input("Enter the MiRNA sequence you want to target:")
+    print(GenerateToehold(seq))
+    
+    # Prevent window from closing if run from a file explorer.
+    input("Press enter to quit...")
+    
